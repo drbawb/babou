23d7e610585011dda89a70bca320e97e9b3ae27d
Robert Straw <drbawb@fatalsyntax.com>
2013-06-21 13:02:52 -0500

    Added basic commandline parsing.

--- babou.go
+++ babou.go
@@ -1,9 +1,47 @@
 package main
 
 import (
-	log "log"
+	fmt "fmt"
+	flag "flag"
 )
 
+type AppSettings struct {
+	Debug *bool
+
+	WebStack *bool
+	TrackerStack *bool
+	FullStack *bool
+
+	WebPort *int
+	TrackerPort *int
+}
+
 func main() {
-	log.Println("babou fast like veyron.")
+	//Output welcome message:
+	fmt.Println("babou fast like veyron.")
+
+	//Parse command line flags
+	appSettings := parseFlags()
+
+	if *appSettings.Debug {
+		fmt.Println("LOGGING DEBUG MESSAGES TO CONSOLE.")
+	}
+}
+
+func parseFlags() *AppSettings {
+	appSettings := &AppSettings{}
+
+	appSettings.Debug = flag.Bool("debug", false, "Logs debug information to console.")
+
+	appSettings.WebStack = flag.Bool("web-stack", false, "Enables the web application server.")
+	appSettings.TrackerStack = flag.Bool("track-stack", false, "Enables the torrent tracker.")
+	appSettings.FullStack = flag.Bool("full-stack", true, "Enables the full application stack. - Disabled if track-stack or web-stack are set.")
+
+	appSettings.WebPort = flag.Int("web-port", 8080, "Sets the web application's port number.")
+	appSettings.TrackerPort = flag.Int("track-port", 4200, "Sets the tracker's listening port number.")
+	
+
+
+	flag.Parse()
+	return appSettings
 }
