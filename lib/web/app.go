23e59b1b8a155f288bee41ed6109176b91d9297d
Robert Straw <drbawb@fatalsyntax.com>
2013-06-23 08:08:58 -0500

    Updated `go doc` documentation.

--- lib/web/app.go
+++ lib/web/app.go
@@ -6,14 +6,17 @@ import (
 	"strings"
 )
 
-// data
-
+// Represents an HTTP response from a `Controller`
+// The middleware or router is responsible for using
+//   this result appopriately.
 type Result struct {
 	Body     []byte //HTTP Response Body
 	Status   int    //HTTP Status Code
 	Redirect *RedirectPath
 }
 
+// Requests an HTTP redirect from the middleware or
+// router.
 type RedirectPath struct {
 	NamedRoute string //or:
 
@@ -21,12 +24,20 @@ type RedirectPath struct {
 	ActionName     string
 }
 
-// A controller must take an action and map it to a Result
-// Results can be created manually or by the app/View helpers.
+// A `Controller` handles a request by taking an action-name and
+//   a map of request parameters from the middleware or router.
+// These results are usually passed to an Actio or otherwise
+//   turned into a servicable `Result` object.
 type Controller interface {
 	HandleRequest(string, map[string]string) *Result
 }
 
+// An action takes a map of request-parameters from the middleware
+//  or router and turns it into a servicable HTTP result.
+type Action func(map[string]string) *Result
+
+// Returns a 404 error if a user asks `babou` for the contents of
+//  a directory.
 func DisableDirectoryListing(h http.Handler) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "" || strings.HasSuffix(r.URL.Path, "/") {
@@ -37,7 +48,3 @@ func DisableDirectoryListing(h http.Handler) http.HandlerFunc {
 		h.ServeHTTP(w, r)
 	})
 }
-
-type Action func(map[string]string) *Result
-
-// functions
